0ffb0ebc32905623db1f828d8c1177f0219c53f3
ethan <ethan@DESKTOP-0GM147B.unl.edu>
2021-08-26 13:46:43 -0500

    Initial Commit

---
+++ src/Hello.java
@@ -0,0 +1,19 @@
+/**
+ * Author: Ethan Riegsecker (eriegsecker2@huskers.unl.edu)
+ * 
+ * Date: 08/26/2021
+ * 
+ * Hello world program that displays our names
+ * 
+ * 
+ * @author ethan
+ *
+ */
+
+
+public class Hello {
+	public static void main(String args[]) {
+		System.out.println("Ethan Riegsecker");
+	}
+}
+	
